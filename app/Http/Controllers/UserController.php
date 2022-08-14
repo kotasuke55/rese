563953edd9f9bdac69f6ec13a660a4cce2933198
kotasuke55 <uverock5@gmail.com>
2022-08-14 22:11:26 +0900

    eight

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Models\Shop;
-use App\Models\Area;
-use App\Models\Genre;
 use App\Models\Like;
 use App\Models\Reserve;
 use Illuminate\Support\Facades\Auth;
@@ -16,9 +13,9 @@ class UserController extends Controller
   public function mypage()
   {
     $user = Auth::user()->name;;
-    $shops = Shop::all();
-    $reserves = Reserve::all();
-    $likes = Like::all();
-    return view('mypage',compact('user','shops','reserves','likes'));
+    $id = Auth::id();
+    $reserves = Reserve::where('user_id',$id)->get();
+    $likes = Like::where('user_id',$id)->get();
+    return view('mypage',compact('user','reserves','likes'));
   }
 }
